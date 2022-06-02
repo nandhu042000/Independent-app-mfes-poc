937b6c4c977d06e57b45925f552ca0803f27424b
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2022-06-02 14:45:37 +0530

    updatign app component

--- student-report/src/app/app.module.ts
+++ student-report/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
+import { ViewsComponent } from './views/views.component';
+import { ColumnsComponent } from './columns/columns.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    FilterComponent,
+    ViewsComponent,
+    ColumnsComponent
   ],
   imports: [
     BrowserModule,
