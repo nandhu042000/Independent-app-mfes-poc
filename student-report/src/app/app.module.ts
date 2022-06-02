a9fa084bca30e07d7b58cc1e19340efcee50fc98
Nandha Kumar <nandhu042000@gmail.com>
2022-06-02 13:54:32 +0530

    student table added

--- student-report/src/app/app.module.ts
+++ student-report/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableReportComponent } from './components/table-report/table-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableReportComponent
   ],
   imports: [
     BrowserModule,
